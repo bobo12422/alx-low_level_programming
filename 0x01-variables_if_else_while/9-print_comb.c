8c5b892859a4e6e80a03c26476c387de7f44572b
Bizimana Bosco <bizimana922@gmail.com>
2023-02-17 07:55:38 -0800

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -4,24 +4,24 @@
  * main - Entry point
  *
  * Return: Always 0 (Success)
- */
+ **/
 int main(void)
 {
-    int num;
+        int num;
 
-    for (num = 0; num <= 9; num++)
-    {
-        putchar(num + '0');
-
-        if (num < 9)
+        for (num = 0; num <= 9; num++)
         {
-            putchar(',');
-            putchar(' ');
+                putchar(num + '0');
+
+                if (num < 9)
+                {
+                        putchar(',');
+                        putchar(' ');
+                }
         }
-    }
 
-    putchar('\n');
+        putchar('\n');
 
-    return (0);
+        return (0);
 }
 
