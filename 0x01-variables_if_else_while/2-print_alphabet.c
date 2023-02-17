94b43011775413ecf9150c62fd2dfb7c1be820e1
Bizimana Bosco <bizimana922@gmail.com>
2023-02-17 05:58:49 -0800

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -9,21 +9,13 @@
  */
 int main(void)
 {
-        char c;
-        char d;
-
-        c = 'a';
-        d = 'A'
-                while
-                        (c <= 'z') {
-                                putchar(c);
-                                c++;
-                        }
-        while
-                (d <= 'Z') {
-                        putchar(d);
-                        d++;
-                }
-        putchar('\n');
-        return(0);
+    char c = 'a';
+    while (c <= 'z')
+    {
+        putchar(c);
+        c++;
+    }
+    putchar('\n');
+    return (0);
 }
+
