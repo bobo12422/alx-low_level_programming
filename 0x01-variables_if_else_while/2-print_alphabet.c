7296a45a06e74ce08563679540624c12e99aa68f
Bizimana Bosco <bizimana922@gmail.com>
2023-02-17 06:01:59 -0800

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,21 +1,21 @@
 #include <stdio.h>
-/* betty style doc for function main goes there */
+
 /**
- * main - Entry point of the program
- *
- * This function prints the alphabet in lowercase and then in uppercase.
+ * main - Entry point
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-    char c = 'a';
-    while (c <= 'z')
-    {
-        putchar(c);
-        c++;
-    }
-    putchar('\n');
-    return (0);
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		putchar(c);
+		c++;
+	}
+
+	putchar('\n');
+	return (0);
 }
 
