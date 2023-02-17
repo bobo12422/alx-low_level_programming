7ef485bc367d965ece2aa313a7f6663e4a9c3b5c
Bizimana Bosco <bizimana922@gmail.com>
2023-02-17 04:42:32 -0800

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+#include <stdio.h>
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point of the program
+ *
+ * This function generates a random number and determines whether it is
+ * positive, negative, or zero. It then prints out the result.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c;
+	char d;
+	
+	c = 'a';
+	d = 'A'
+		while 
+			(c <= 'z') {
+				putchar(c);
+				c++;
+			}
+	while 
+		(d <= 'Z') {
+			putchar(d);
+			d++;
+		}
+	putchar('\n');
+	return(0);
