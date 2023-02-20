3b6316ddd8044fd6900817433c18a8590808e886
Bizimana Bosco <bizimana922@gmail.com>
2023-02-19 18:41:03 -0800

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -12,14 +12,15 @@ for (i = 0; i <= 98; i++)
 {
 for (j = i + 1; j <= 99; j++)
 {
-putchar((i / 10) +  putchar((i % 10) + '0');
+putchar((i / 10) + '0');
+putchar((i % 10) + '0');
 putchar(' ');
 putchar((j / 10) + '0');
 putchar((j % 10) + '0');
 if (i != 98 || j != 99)
 {
 putchar(',');
-putchar(' ')
+putchar(' ');
 }
 }
 }
