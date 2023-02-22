ea0ffb95d003bf74baf51024b1d5bf8f07312191
Bizimana Bosco <bizimana922@gmail.com>
2023-02-21 23:14:18 -0800

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -5,35 +5,35 @@
  */
 void times_table(void)
 {
-    int i, j;
+int i, j;
 
-    for (i = 0; i <= 9; i++)
-    {
-        for (j = 0; j <= 9; j++)
-        {
-            int res = i * j;
-            if (j == 0)
-            {
-                _putchar('0');
-            }
-            else if (res < 10)
-            {
-                _putchar(' ');
-                _putchar(res + '0');
-            }
-            else
-            {
-                _putchar(res / 10 + '0');
-                _putchar(res % 10 + '0');
-            }
+for (i = 0; i <= 9; i++)
+{
+for (j = 0; j <= 9; j++)
+{
+int res = i * j;
+if (j == 0)
+{
+_putchar('0');
+}
+else if (res < 10)
+{
+_putchar(' ');
+_putchar(res + '0');
+}  
+else
+{
+_putchar(res / 10 + '0');
+_putchar(res % 10 + '0');
+}
 
-            if (j != 9)
-            {
-                _putchar(',');
-                _putchar(' ');
-            }
-        }
-        _putchar('\n');
-    }
+if (j != 9)
+{
+_putchar(',');
+_putchar(' ');
+}
+}
+_putchar('\n');
+}
 }
 
