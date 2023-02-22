95deb5dc568e286d1cc7a57dbc057e0fdb47723e
Bizimana Bosco <bizimana922@gmail.com>
2023-02-21 19:46:48 -0800

    9-times_table.c

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * times_table - prints the 9 times table, starting with 0
+ */
+void times_table(void)
+{
+    int i, j;
+
+    for (i = 0; i <= 9; i++)
+    {
+        for (j = 0; j <= 9; j++)
+        {
+            int res = i * j;
+            if (j == 0)
+            {
+                _putchar('0');
+            }
+            else if (res < 10)
+            {
+                _putchar(' ');
+                _putchar(res + '0');
+            }
+            else
+            {
+                _putchar(res / 10 + '0');
+                _putchar(res % 10 + '0');
+            }
+
+            if (j != 9)
+            {
+                _putchar(',');
+                _putchar(' ');
+            }
+        }
+        _putchar('\n');
+    }
+}
+
