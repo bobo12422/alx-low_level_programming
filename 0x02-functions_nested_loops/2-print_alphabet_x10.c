3109ebb96686828559898879ec01ac0c6f194a08
Bizimana Bosco <bizimana922@gmail.com>
2023-02-21 18:31:07 -0800

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
-i * print_alphabet_x10 - Prints the alphabet in lowercase 10 times.
+ * print_alphabet_x10 - Prints the alphabet in lowercase 10 times.
  *
  * Return: void.
  */
