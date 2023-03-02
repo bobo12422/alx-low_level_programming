a3788272c8be7c7c721bd8825a754031b22bacea
Bizimana Bosco <bizimana922@gmail.com>
2023-03-01 18:57:17 -0800

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,28 +1,23 @@
-#include "holberton.h"
+#include "main.h"                                                                                                                    
 
-/**
-* _strcat - Concatenates two strings.
-* @dest: Pointer to destination string.
-* @src: Pointer to source string.
-*
-* Return: Pointer to resulting string.
-*/
-char *_strcat(char *dest, char *src)
-{
-int i, j;
+/**                                                                                                                                  
+* strcat - Concatenates the string pointed to by @src, including the x                                                     
+*          null byte, to the end of the string pointed to by @dest.                                                                 
+* @dest: A pointer to the string to be concatenated upon.                                                                           
+* @src: The source string to be appended to @dest.                                                                                  
+*                                                                                                                                   
+* Return: A pointer to the destination string @dest.                                                                                
+*/                                                                                                                                  
+char *_strcat(char *dest, const char *src)                                                                                            
+{                                                                                                                                    
+int index = 0, dest_len = 0;                                                                                                 
 
-i = j = 0;
-while (dest[i] != '\0')
-i++;
+while (dest[index++])                                                                                                        
+dest_len++;                                                                                                          
 
-while (src[j] != '\0')
-{
-dest[i] = src[j];
-i++;
-j++;
-}
-dest[i] = '\0';
+for (index = 0; src[index]; index++)                                                                                         
+dest[dest_len++] = src[index];                                                                                       
 
-return (dest);
+return (dest);                                                                                                               
 }
 
