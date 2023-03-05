a1baeece7ab545f2d2996dc32e2cb41aa551c3e9
Bosco <bizimana922@gmail.com>
2023-03-05 06:30:40 -0800

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,39 +1,38 @@
 #include <stdio.h>
-
 /**
- * main - Entry point
- *
- * Description: Prints all possible combinations of three digits,
- * separated by a comma and a space. The digits must be different
- * and in ascending order.
- *
- * Return: Always 0 (Success)
- */
+* main - Entry point
+*
+* Description: Prints all possible combinations of three digits,
+* separated by a comma and a space. The digits must be different
+* and in ascending order.
+*
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
-    int i, j, k;
+int i, j, k;
 
-    for (i = 0; i <= 7; i++)
-    {
-        for (j = i + 1; j <= 8; j++)
-        {
-            for (k = j + 1; k <= 9; k++)
-            {
-                putchar(i + '0');
-                putchar(j + '0');
-                putchar(k + '0');
+for (i = 0; i <= 7; i++)
+{
+for (j = i + 1; j <= 8; j++)
+{
+for (k = j + 1; k <= 9; k++)
+{
+putchar(i + '0');
+putchar(j + '0');
+putchar(k + '0');
 
-                if (i != 7 || j != 8 || k != 9)
-                {
-                    putchar(',');
-                    putchar(' ');
-                }
-            }
-        }
-    }
+if (i != 7 || j != 8 || k != 9)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
 
-    putchar('\n');
+putchar('\n');
 
-    return (0);
+return (0);
 }
 
