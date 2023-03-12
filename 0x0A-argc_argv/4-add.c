18ada2ee5a49b665ac94107180717466cdd06636
Bosco <bizimana922@gmail.com>
2023-03-12 07:09:59 -0700

    4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -3,30 +3,36 @@
 #include <ctype.h>
 #include <string.h>
 
-/**
- * check_num - check - string there are digit
- * @str: array str
- *
- * Return: Always 0 (Success)
- */
+int check_num(char *str);
+
+int main(int argc, char *argv[])
+{
+if (argc < 2) {
+printf("Usage: %s <number>\n", argv[0]);
+return 1;
+}
+
+if (check_num(argv[1])) {
+printf("%s is a valid number\n", argv[1]);
+} else {
+printf("%s is not a valid number\n", argv[1]);
+}
+
+return 0;
+}
 
 int check_num(char *str)
-
 {
-/*Declaring variables*/
-unsigned int count;
+unsigned int count = 0;
 
-count = 0;
-while (count < strlen(str)) /*count string*/
-
-{
-if (!isdigit(str[count])) /*check if str there are digit*/
-{
-return (0);
+while (count < strlen(str)) {
+if (!isdigit(str[count])) {
+return 0;
 }
 
 count++;
 }
-return (1);
+
+return 1;
 }
 
