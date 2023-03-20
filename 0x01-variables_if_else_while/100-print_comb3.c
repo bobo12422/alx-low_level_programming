e7ddbc22a4a607d1ce7dd70d9e5bbde793ce5ce6
Bosco <bizimana922@gmail.com>
2023-03-20 09:33:21 -0700

    0-object_like_macro.h

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,36 +1,31 @@
-#include <stdio.h>
+#include<stdio.h>
 
 /**
-* main - prints all possible combinations of two digits
-* Return: 0 on success
+*main-Entrypoint
+*
+*Return:Always0(Success)
 */
-int main(void)
+intmain(void)
 {
-int i, j;
+intnum1,num2;
 
-for (i = 0; i < 90; i++)
+for(num1=0;num1<10;num1++)
 {
-for (j = i + 1; j < 100; j++)
+for(num2=num1+1;num2<10;num2++)
 {
-if (i != j)
-{
-putchar((i / 10) + '0');
-putchar((i % 10) + '0');
-putchar(' ');
-putchar((j / 10) + '0');
-putchar((j % 10) + '0');
+putchar(num1+'0');
+putchar(num2+'0');
 
-if (i != 98 || j != 99)
-{
+if(num1==8&&num2==9)
+break;
+
 putchar(',');
-putchar(' ');
-}
-}
+putchar('');
 }
 }
 
 putchar('\n');
 
-return (0);
+return(0);
 }
 
