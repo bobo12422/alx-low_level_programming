7af1deb99ceefe69f759c5f672f654b57e41c438
Bosco <bizimana922@gmail.com>
2023-04-03 21:13:07 -0700

    C - More singly linked lists

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -7,9 +7,10 @@
 *
 * Return: address of the new element or NULL if it fails
 */
+
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
-listint_t *new_node, *temp;
+listint_t *new_node, **last;
 
 new_node = malloc(sizeof(listint_t));
 if (new_node == NULL)
@@ -18,17 +19,11 @@ return (NULL);
 new_node->n = n;
 new_node->next = NULL;
 
-if (*head == NULL)
-{
-*head = new_node;
-return (new_node);
-}
+last = head;
+while (*last != NULL)
+last = &(*last)->next;
 
-temp = *head;
-while (temp->next != NULL)
-temp = temp->next;
-
-temp->next = new_node;
+*last = new_node;
 return (new_node);
 }
 
