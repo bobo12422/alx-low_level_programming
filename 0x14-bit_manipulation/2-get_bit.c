6eab0ba58085e801138f53d56bde0e97223f6c20
Bosco <bizimana922@gmail.com>
2023-04-09 00:48:53 -0700

    correcting error: are not necessary for single statement blocks

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -10,9 +10,8 @@
 
 int get_bit(unsigned long int n, unsigned int index)
 {
-if (index >= sizeof(unsigned long int) * 8) {
+if (index >= sizeof(unsigned long int) * 8)
 return (-1);
-}
 
 return ((n >> index) & 1);
 }
