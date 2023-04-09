4a4e3ec214ec82039aa5fb76546eea4c40f15db0
Bosco <bizimana922@gmail.com>
2023-04-09 01:37:20 -0700

    Function that checks the endianness

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -1,22 +1,15 @@
 #include "main.h"
 
 /**
-* set_bit - sets a bit at a given index to 1
-* @n: pointer to the number to change
-* @index: index of the bit to set to 1
-*
-* Return: 1 for success, -1 for failure
+* get_endianness - checks if a machine is little or big endian
+* Return: 0 for big, 1 for little
 */
 
-int set_bit(unsigned long int *n, unsigned int index)
+int get_endianness(void)
 {
-if (index > 63)
-{
-return (-1);
-}
+unsigned int i = 1;
+char *c = (char *) &i;
 
-*n |= (1UL << index); /* set the bit to 1 using bitwise OR */
-
-return (1);
+return (*c);
 }
 
